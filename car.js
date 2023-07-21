e5a2f6315efc465e2655c0938a7b268b17de7d88
visheshpawar <128835080+visheshpawar@users.noreply.github.com>
2023-07-21 12:06:48 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,83 @@
+
+function cng()
+{
+    
+    document.getElementById("a1").src="ef.png";
+
+}
+function cng1()
+{
+  
+    document.getElementById("a1").src="af.png";
+   
+}
+function cngg()
+{
+    
+    document.getElementById("a2").src="qw.png";
+}
+function cngg1()
+{
+  
+    document.getElementById("a2").src="wq.png";
+   
+}
+function cngg2()
+{
+    
+    document.getElementById("a3").src="rr.png";
+}
+function cngg22()
+{
+  
+    document.getElementById("a3").src="r.png";
+   
+}
+function cngg21()
+{
+    
+    document.getElementById("a4").src="mn.png";
+}
+function cngg222()
+{
+  
+    document.getElementById("a4").src="nm.png";
+   
+}
+function cngg211()
+{
+    
+    document.getElementById("a5").src="zx.png";
+}
+function cngg2222()
+{
+  
+    document.getElementById("a5").src="xz.png";
+   
+}
+function cmrt()
+{
+    document.getElementById("gfg").style.width="20vw";
+}
+function fgh()
+{
+    document.getElementById("gfg").style.width="0vw";
+}
+let video=document.getElementById("ertt");
+document.addEventListener("scroll",function()
+{
+let cv= 1+ window.scrollY/-600;
+video.style.opacity=cv;
+
+
+}
+)
+let vdf=document.getElementById("gfs");
+document.addEventListener("scroll",function()
+{
+let cvv= 1+ window.scrollY/600;
+vdf.style.opacity=cvv;
+
+
+}
+)
